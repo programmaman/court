b1c48620d9cf1838c2ac1ba47c33a02b8483f2c1
alec <alecbraynen@usf.edu>
2025-03-07 18:36:16 -0500

    Begin Adding Logging

--- src/api/side-chain/index.js
+++ src/api/side-chain/index.js
@@ -6,6 +6,17 @@ export * from "./chain-params";
 
 export { default as requestSwitchChain } from "./request-switch-chain";
 
+// Secure logging function
+const log = (message, data = {}) => {
+  if (process.env.NODE_ENV === "development") {
+    console.log(`[Index] ${message}`, data);
+  }
+};
+
+log("Initializing requestWatchToken");
+
 export const requestWatchToken = createWatchToken({
   getChainParams: getSideChainParams,
 });
+
+log("requestWatchToken initialized successfully");
