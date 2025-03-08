c2b28af2d627f26bf0fcd46bf9c01d0c4401e105
alec <alecbraynen@usf.edu>
2025-03-08 13:31:15 -0500

    Add logging to subgraph.js

--- src/bootstrap/subgraph.js
+++ src/bootstrap/subgraph.js
@@ -1,6 +1,12 @@
+import log from "/../helpers/logger"; // Import logger
+
+log.debug("Initializing subgraph configuration...");
+
 export const displaySubgraph = {
-  1: process.env.REACT_APP_SUBGRAPH_MAINNET_DISPLAY,
-  100: process.env.REACT_APP_SUBGRAPH_GNOSIS_DISPLAY,
+  1: process.env.REACT_APP_SUBGRAPH_MAINNET_DISPLAY || log.warn("Missing subgraph URL for Mainnet"),
+  100: process.env.REACT_APP_SUBGRAPH_GNOSIS_DISPLAY || log.warn("Missing subgraph URL for Gnosis"),
   10200: "https://api.studio.thegraph.com/query/61738/kleros-display-chiado/version/latest",
   11155111: "https://api.studio.thegraph.com/query/61738/kleros-display-sepolia/version/latest",
 };
+
+log.debug("Subgraph configuration loaded:", displaySubgraph);
