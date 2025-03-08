2c586d73fa0f2cd631429507d0ae1f3e261580c8
alec <alecbraynen@usf.edu>
2025-03-08 12:48:09 -0500

    Add logging to index.js

--- src/api/side-chain/index.js
+++ src/api/side-chain/index.js
@@ -1,22 +1,16 @@
+import log from "../../helpers/logger";
 import createWatchToken from "./create-watch-token";
 import { getSideChainParams } from "./chain-params";
 
 export { SideChainApiProvider, useSideChainApi } from "./react-adapters";
 export * from "./chain-params";
-
 export { default as requestSwitchChain } from "./request-switch-chain";
 
-// Secure logging function
-const log = (message, data = {}) => {
-  if (process.env.NODE_ENV === "development") {
-    console.log(`[Index] ${message}`, data);
-  }
-};
-
-log("Initializing requestWatchToken");
+// Use loglevel for debugging
+log.debug("Initializing requestWatchToken");
 
 export const requestWatchToken = createWatchToken({
   getChainParams: getSideChainParams,
 });
 
-log("requestWatchToken initialized successfully");
+log.debug("requestWatchToken initialized successfully");
