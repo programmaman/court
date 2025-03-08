082adcfe2e7230046c8c914d31769846929c4520
alec <alecbraynen@usf.edu>
2025-03-08 07:40:02 -0500

    Revert "Add Logging to button.js"
    This reverts commit 94e2a066e5699c38123aa267bf1df419ac36eebe.

--- src/adapters/antd/button.js
+++ src/adapters/antd/button.js
@@ -3,29 +3,14 @@ import t from "prop-types";
 import styled from "styled-components/macro";
 import { Button as AntdButton } from "antd";
 
-// Use a logging library or console.log for development
-const log = (message, data = {}) => {
-  if (process.env.NODE_ENV === "development") {
-    console.log(`[Button Component] ${message}`, data);
-  }
-};
-
 /**
  * Attempt to tackle props passing issue.
  * @see { @link https://github.com/ReactTraining/react-router/issues/6962 }
  */
-export function ButtonLink({ children, className, ...props }) {
-  log("ButtonLink rendered", { className, props });
 
+export function ButtonLink({ children, className, ...props }) {
   return (
-    <StyledButtonLink
-      {...getForwardedProps(props)}
-      className={className}
-      onClick={(e) => {
-        log("ButtonLink clicked", { event: e, props });
-        props.onClick?.(e);
-      }}
-    >
+    <StyledButtonLink {...getForwardedProps(props)} className={className}>
       {children}
     </StyledButtonLink>
   );
@@ -37,17 +22,8 @@ ButtonLink.propTypes = {
 };
 
 export function Button({ children, className, ...props }) {
-  log("Button rendered", { className, props });
-
   return (
-    <StyledButton
-      {...getForwardedProps(props)}
-      className={className}
-      onClick={(e) => {
-        log("Button clicked", { event: e, props });
-        props.onClick?.(e);
-      }}
-    >
+    <StyledButton {...getForwardedProps(props)} className={className}>
       {children}
     </StyledButton>
   );
@@ -57,10 +33,7 @@ Button.propTypes = AntdButton.propTypes;
 
 const pick = (keys) => (obj) =>
   keys.reduce(
-    (acc, key) =>
-      Object.prototype.hasOwnProperty.call(obj, key)
-        ? Object.assign(acc, { [key]: obj[key] })
-        : acc,
+    (acc, key) => (Object.prototype.hasOwnProperty.call(obj, key) ? Object.assign(acc, { [key]: obj[key] }) : acc),
     {}
   );
 
@@ -79,14 +52,11 @@ const getForwardedProps = pick([
   "block",
 ]);
 
-const StyledButtonLink = styled(AntdButton).attrs((props) => ({
-  ...props,
-  type: "link",
-}))`
-    &.ant-btn-link {
-        padding-left: 0;
-        padding-right: 0;
-    }
+const StyledButtonLink = styled(AntdButton).attrs((props) => ({ ...props, type: "link" }))`
+  &.ant-btn-link {
+    padding-left: 0;
+    padding-right: 0;
+  }
 `;
 
 const StyledButton = styled(AntdButton).attrs((props) => ({ ...props }))``;
